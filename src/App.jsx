928d71bccede25159ee82d0486a62b2dde422dd6
Cleve-keem <belloharyourmidey@gmail.com>
2025-08-23 16:01:15 -0500

    added heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Welcome to Sound Foundation Tech School</h1>;
+  return (
+    <h1 className="font-semibold text-center text-4xl mt-4">
+      Welcome to Sound Foundation Tech School
+    </h1>
+  );
 }
 
 export default App;
