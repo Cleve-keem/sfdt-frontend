c80104ddc536f9d64531f97fd25afc460f9b672e
Mubarak <adebowaleraji0103@gmail.com>
2025-08-28 07:36:12 +0100

    d

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -4,7 +4,6 @@ const images = [
   "public/shoes (1).jpg",
   "public/shoes (3).jpg",
   "public/shoes (4).jpg",
-
 ];
 
 const HeroSlider = () => {
@@ -18,17 +17,19 @@ const HeroSlider = () => {
   }, []);
 
   return (
-    <div className="relative w-full h-screen overflow-hidden ">
-      {images.map((img, index) => (
-        <img
-          key={index}
-          src={img}
-          alt=""
-          className={`absolute inset-0 w-full h-full py-[2rem] object-cover transition-opacity duration-1000 ease-in-out ${
-            index === current ? "opacity-100" : "opacity-0"
-          }`}
-        />
-      ))}
+    <div>
+      <div className="relative w-full h-screen overflow-hidden ">
+        {images.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt=""
+            className={`absolute inset-0 w-full h-full py-[2rem] object-cover transition-opacity duration-1000 ease-in-out ${
+              index === current ? "opacity-100" : "opacity-0"
+            }`}
+          />
+        ))}
+      </div>
     </div>
   );
 };
