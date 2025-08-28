4f897df2a1c58e0f57f755ae0acb523a8ae1d3dd
Mubarak <adebowaleraji0103@gmail.com>
2025-08-28 08:00:15 +0100

    d

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -17,8 +17,8 @@ const HeroSlider = () => {
   }, []);
 
   return (
-    <div>
-      <div className="relative w-full h-screen overflow-hidden ">
+    <div className="mb-90 relative ">
+      <div className=" w-full h-screen overflow-hidden ">
         {images.map((img, index) => (
           <img
             key={index}
@@ -29,6 +29,13 @@ const HeroSlider = () => {
             }`}
           />
         ))}
+      </div>
+      <div className="absolute inset-0 flex flex-col justify-center px-5 bg-black/40  md:px-30">
+        <h1 className="text-3xl font-bold text-yellow-300 md:text-4xl md:w-3xl">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus,
+          excepturi!
+        </h1>
+        <button className="bg-red-500 w-fit px-4 py-1.5">Enroll</button>
       </div>
     </div>
   );
