b7b0b4071816c8cc0b4ba7864af6a7f95bc30dd1
Mubarak <adebowaleraji0103@gmail.com>
2025-09-01 19:23:25 +0100

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Navbar from "./components/Navbars/Navbar";
 import HeroSlider from "./components/HeroSlider";
-import Footer from "./components/Footer";
-import CourseCards from "./components/CourseCards";
 import ProfileBar from "./components/ProfileBar";
 import WelcomeText from "./components/WelcomeText";
+import CourseCards from "./components/CourseCards";
 import LearningPaths from "./components/LearningPaths";
+import Footer from "./components/Footer";
 
 function App() {
   return (
