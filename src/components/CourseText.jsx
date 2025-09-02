501786e7da95ca0bcac230f7ba8077a21081695e
Daniel Sanu <sanudaniel46@gmail.com>
2025-09-01 16:03:49 -1000

    modified the text

--- src/components/CourseText.jsx
+++ src/components/CourseText.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function CourseText() {
   return (
     <section className="bg-black text-white text-center pt-20 px-4">
-      <div className=" flex flex-col lg:items-start gap-1 max-w-[1200px] mx-auto lg:pl-10">
+      <div className=" flex flex-col lg:items-start  max-w-[1200px] mx-auto lg:pl-10">
         <h2 className="text-xl font-bold mb-4">What to learn next</h2>
-        <p className=" text-[15px]">
+        <p className=" text-[15px] w-[624px] text-start">
           When selecting IT courses, consider your current skills, career
           aspirations, and the demand for specific skills in the job market
         </p>
